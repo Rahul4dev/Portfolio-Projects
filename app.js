e5990567a3311952c88337c3dc898184a72efc60
Rahul4dev <rahulsinghsignin@gmail.com>
2023-03-25 23:42:33 +0530

    Added contact page

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
-const sections = document.querySelectorAll(".section");
-const controlContainer = document.querySelector(".controls");
-const controlBtns = document.querySelectorAll(".control");
-const body = document.querySelector(".body");
+const sections = document.querySelectorAll('.section');
+const controlContainer = document.querySelector('.controls');
+const controlBtns = document.querySelectorAll('.control');
+const body = document.querySelector('.body');
+const themeBtn = document.querySelector('.theme-btn');
+/** Banner styles */
+const bannerTextEl = document.getElementById('bannerText');
+const text = 'I love FrontEnd and Designing';
+
+let index = 1;
+let speed = 300;
+
+writeText();
+
+themeBtn.addEventListener('click', (e) => {
+  e.target.classList.toggle('active');
+  let pageColor = document.body;
+  pageColor.classList.toggle('light-mode');
+});
+
+function writeText() {
+  bannerTextEl.innerText = text.slice(0, index);
+
+  index++;
+
+  if (index > text.length) {
+    index = 1;
+  }
+
+  setTimeout(writeText, speed);
+}
 
 function pageTransition() {
   // Button click => active class
@@ -17,46 +44,32 @@ function pageTransition() {
 
   // same using for loop
   for (let i = 0; i < controlBtns.length; i++) {
-    controlBtns[i].addEventListener("click", function () {
-      let btns = document.querySelectorAll(".active-btn");
-      btns[0].className = btns[0].className.replace("active-btn", "");
-      this.className += " active-btn"; // this keyword  doesn't work in arrow functions
+    controlBtns[i].addEventListener('click', function () {
+      let btns = document.querySelectorAll('.active-btn');
+      btns[0].className = btns[0].className.replace('active-btn', '');
+      this.className += ' active-btn'; // this keyword  doesn't work in arrow functions
     });
   }
 
   // section active with click
-  body.addEventListener("click", (e) => {
+  body.addEventListener('click', (e) => {
     const id = e.target.dataset.id;
     if (id) {
       // remove selected section active class
       controlBtns.forEach((btn) => {
-        btn.classList.remove("active");
+        btn.classList.remove('active');
       });
-      e.target.classList.add("active");
+      e.target.classList.add('active');
 
       // hide other sections
       sections.forEach((section) => {
-        section.classList.remove("active");
+        section.classList.remove('active');
       });
 
       const element = document.getElementById(id);
-      element.classList.add("active");
+      element.classList.add('active');
     }
   });
 }
 
 pageTransition();
-
-// (function () {
-//     [...document.querySelectorAll(".control")].forEach(button => {
-//         button.addEventListener("click", function() {
-//             document.querySelector(".active-btn").classList.remove("active-btn");
-//             this.classList.add("active-btn");
-//             document.querySelector(".active").classList.remove("active");
-//             document.getElementById(button.dataset.id).classList.add("active");
-//         })
-//     });
-//     document.querySelector(".theme-btn").addEventListener("click", () => {
-//         document.body.classList.toggle("light-mode");
-//     })
-// })();
